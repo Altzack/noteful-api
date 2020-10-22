d3e530bdc58051722944bda30cdd3d7e69bb4689
Zack <altschulerzack@yahoo.com>
2020-10-21 18:19:03 -0700

    production changes

--- test/endpoint.spec.js
+++ test/endpoint.spec.js
@@ -3,40 +3,44 @@ const knex = require("knex");
 const app = require("../src/app");
 const { makeFolderArray } = require("./folderfixtures");
 
-describe("Folders Endpoints", function () {
+describe("Noteful Endpoints", function () {
   let db;
 
   before("make knex instance", () => {
     db = knex({
       client: "pg",
-      connection: process.env.TEST_DB_URL,
+      connection: process.env.TEST_DATABASE_URL,
     });
     app.set("db", db);
   });
 
   after("disconnect from db", () => db.destroy());
 
-  before("clean the table", () => db("folders").truncate());
+  before("clean the table", () => db.raw("TRUNCATE folders, notes"));
 
-  afterEach("cleanup", () => db("folders").truncate());
-});
+  afterEach("cleanup", () => db.raw("TRUNCATE folders, notes"));
 
-describe("Get /api/folders Endpoint", function () {
-  let db;
-  context("Given database is empty", () => {
-    it(`responds with 200 and an empty list`, () => {
-      return supertest(app).get("/api/folders").expect(200, []);
+  describe("Get /api/folders Endpoint", function () {
+    context("Given database is empty", () => {
+      it(`responds with 200 and an empty list`, () => {
+        return supertest(app).get("/api/folders").expect(200, []);
+      });
     });
-  });
-  context("Given there are folders in the database", () => {
-    const testFolders = makeFolderArray();
+    context("Given there are folders in the database", () => {
+      const testFolders = makeFolderArray();
 
-    beforeEach("insert folders", () => {
-      return db.into("folders").insert(testFolders);
-    });
+      beforeEach("insert folders", () => {
+        return db
+          .into("notes")
+          .insert(testFolders)
+          .then(() => {
+            return db.into("folders").insert(testFolders);
+          });
+      });
 
-    it("gets the folders from the store", () => {
-      return supertest(app).get("/api/folders").expect(200, testFolders);
+      it("gets the folders from the store", () => {
+        return supertest(app).get("/api/folders").expect(200, testFolders);
+      });
     });
   });
 });
